85fd01965cc526243e8eb324b37bf4033d3e7d5b
RyeinKim <ryein6896@gmail.com>
2024-08-13 16:48:54 +0900

    POST work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('users')
+  createUser(@Body('name') name: string, @Body('email') email: string): string {
+    return `${name} ${email}`;
   }
 }
