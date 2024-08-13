0652b9f29810bba494b2c3fa6c20afdfdc177a97
RyeinKim <ryein6896@gmail.com>
2024-08-13 17:03:03 +0900

    3_REST work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, HttpCode, Patch, Post, Put} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,12 +6,45 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRequest(): string {
+    return 'GET 요청에 성공했습니다.';
   }
 
-  @Post('users')
-  createUser(@Body('name') name: string, @Body('email') email: string): string {
-    return `${name} ${email}`;
+  @Post()
+  @HttpCode(201)
+  postRequest(@Body() body: any) {
+    return {
+      status: 201,
+      message: 'POST 요청에 성공했습니다.',
+      data: body,
+    };
+  }
+
+  @Put()
+  putRequest(@Body() body: any) {
+    return {
+      status: 200,
+      message: 'PUT 요청에 성공했습니다.',
+      data: body,
+    };
+  }
+
+  @Patch()
+  patchRequest(@Body() body: any) {
+    return {
+      status: 200,
+      message: 'PATCH 요청에 성공했습니다.',
+      data: body,
+    };
+  }
+
+  @Delete()
+  @HttpCode(204)
+  deleteRequest() {
+    return {
+      status: 204,
+      message: 'DELETE 요청에 성공했습니다.',
+      data: null,
+    };
   }
 }
