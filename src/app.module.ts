2baf8fd5f6dfbd7594d212939704efcd0cdd6395
RyeinKim <ryein6896@gmail.com>
2024-08-14 15:25:30 +0900

    5_SQL_1 work

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,16 @@ import { CommentController } from './comment/comment.controller';
 import { UserService } from './user/user.service';
 import { PostService } from './post/post.service';
 import { CommentService } from './comment/comment.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: '.env.dev',
+      }),
+      DatabaseModule,
+  ],
   controllers: [AppController, UserController, PostController, CommentController],
   providers: [AppService, UserService, PostService, CommentService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('BACKEND_PORT') || 3000;
 
   app.enableCors({
     origin: '*',
@@ -12,7 +16,7 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  await app.listen(3000);
-  console.log(`[HTTP] app is running on: http://localhost:3000`);
+  await app.listen(PORT);
+  console.log(`[HTTP] app is running on: http://localhost:${PORT}`);
 }
 bootstrap();
