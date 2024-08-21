7a67c9a5e30d1891981cfcb2bb08e194a999d9d4
RyeinKim <ryein6896@gmail.com>
2024-08-21 14:37:46 +0900

    [feat] helmet 변환 완료

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {TimeoutInterceptor} from "./common/interceptor/timeout.interceptor";
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 글로벌 Interceptor 설정
   app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  app.use(helmet());
 
   await app.listen(3000);
 }
