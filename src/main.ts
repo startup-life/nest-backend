3f278e502dfa996e168f34783b9e5f86752171e3
RyeinKim <ryein6896@gmail.com>
2024-08-21 14:31:38 +0900

    [Draft] timeout 적용 (추후 throttler, 적용 필요)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { TimeoutInterceptor } from './common/interceptor/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
   })
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // 글로벌 Interceptor 설정
+  app.useGlobalInterceptors(new TimeoutInterceptor());
 
   await app.listen(3000);
   console.log(`[HTTP] app is running on: http://localhost:3000`);
