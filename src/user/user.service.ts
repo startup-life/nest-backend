3dbc594a0c4652ff3976bd1a7009822a406b744e
RyeinKim <ryein6896@gmail.com>
2024-08-23 16:04:43 +0900

    [fix] updateUser 서비스 코드 오류 해결

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -82,22 +82,22 @@ export class UserService {
         // 유저 정보 조회
         const user = await this.getUserById(userId);
 
-        const savedUser = await this.userRepository.update(userId, { nickname });
-        if (!savedUser) throw new NotFoundException('not found user');
+        // 닉네임 수정
+        if (nickname) {
+            user.nickname = nickname;
+        }
 
         // 프로필 이미지 경로가 없는 경우
         if (!profileImagePath) {
             // 프로필 이미지 삭제
-            await this.userRepository.update(userId, { fileId: null });
-            return await this.getUserById(userId);
+            user.fileId = null;
+        } else if (profileImagePath !== user.profileImagePath) {
+            // 새로운 프로필 이미지 경로 저장
+            const profileImage = await this.createProfileImage(user.userId, profileImagePath);
+            user.fileId = profileImage.fileId;
         }
 
-        // 기존 프로필 이미지 경로와 같은 경우
-        if (profileImagePath === user.profileImagePath) return await this.getUserById(userId);
-
-        // 새로운 프로필 이미지 경로 저장
-        const profileImage = await this.createProfileImage(user.userId, profileImagePath);
-        user.fileId = profileImage.fileId;
+        // 변경된 유저 정보 저장
         await this.userRepository.save(user);
 
         return await this.getUserById(userId);
