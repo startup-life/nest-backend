bcc03205f394187b3b21479b90c583e9bbcc240d
RyeinKim <ryein6896@gmail.com>
2024-08-30 11:29:41 +0900

    feat: comment 디렉토리 swagger 작성

--- src/comment/dto/add-comment.dto.ts
+++ src/comment/dto/add-comment.dto.ts
@@ -1,6 +1,15 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AddCommentDto {
+    @ApiProperty({
+        type: String,
+        description: '댓글 내용 (1~1000자)',
+        example: '댓글 내용입니다.',
+        required: true,
+        minLength: 1,
+        maxLength: 1000,
+    })
     @IsNotEmpty()
     @IsString()
     @Length(1, 1000)
